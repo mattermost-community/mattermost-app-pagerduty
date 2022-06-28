1c34082c9f337b1aaa6f9e771a3adedaa5f725be
JC Santiago <santiago.benavides-ext@dominion-global.com>
2022-06-27 23:45:24 -0500

    feat: added event Reassigned

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -7,7 +7,7 @@ import {
     AppContext,
     AppContextAction, Incident,
     IncidentWebhook,
-    PostCreate,
+    PostCreate, ReassignedWebhook,
     WebhookEvent,
     WebhookRequest
 } from '../types';
@@ -177,10 +177,40 @@ async function notifyIncidentAcknowledged({ data: { event }, rawQuery }: Webhook
     await mattermostClient.createPost(payload);
 }
 
+async function notifyIncidentReassigned({ data: { event }, rawQuery }: WebhookRequest<WebhookEvent<ReassignedWebhook>>, context: AppContext) {
+    const mattermostUrl: string | undefined = context.mattermost_site_url;
+    const botAccessToken: string | undefined = context.bot_access_token;
+    const eventData: AcknowledgedWebhook = event.data;
+
+    const parsedQuery: ParsedQuery = queryString.parse(rawQuery);
+    const channelId: string = <string>parsedQuery['channelId'];
+
+    const  assignees: string[] = eventData.assignees.map((assign) => hyperlink(assign.summary, assign.html_url));
+    const payload: PostCreate = {
+        message: '',
+        channel_id: channelId,
+        props: {
+            attachments: [
+                {
+                    text: `Reassigned ${hyperlink(`[#${eventData.number}] ${eventData.title}`, eventData.html_url)} to ${assignees.join(', ')} by ${hyperlink(event.agent.summary, event.agent.html_url)}`
+                }
+            ]
+        }
+    };
+
+    const mattermostOptions: MattermostOptions = {
+        mattermostUrl: <string>mattermostUrl,
+        accessToken: <string>botAccessToken
+    };
+    const mattermostClient: MattermostClient = new MattermostClient(mattermostOptions);
+    await mattermostClient.createPost(payload);
+}
+
 const WEBHOOKS_ACTIONS: { [key: string]: Function } = {
     'incident.triggered': notifyIncidentTriggered,
     'incident.annotated': notifyIncidentAnnotated,
-    'incident.acknowledged': notifyIncidentAcknowledged
+    'incident.acknowledged': notifyIncidentAcknowledged,
+    'incident.reassigned': notifyIncidentReassigned
 };
 
 export const incomingWebhook = async (request: Request, response: Response) => {
@@ -189,7 +219,7 @@ export const incomingWebhook = async (request: Request, response: Response) => {
 
     let callResponse: AppCallResponse;
     try {
-        console.log('webhookRequest', webhookRequest.data.event);
+        console.log('webhook', webhookRequest.data.event)
         const action: Function = WEBHOOKS_ACTIONS[webhookRequest.data.event.event_type];
         if (action) {
             await action(webhookRequest, context);
