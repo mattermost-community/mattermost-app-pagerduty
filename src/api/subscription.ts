4737ce02940f88144fb002d0146442ced9000211
Lizeth García <lizeth.garcia@ancient.mx>
2022-12-05 12:48:03 -0600

    feat: added filter to just show subscriptions related to the MM channel we are working with

--- src/api/subscription.ts
+++ src/api/subscription.ts
@@ -37,29 +37,12 @@ export const subscriptionDeleteSubmit: CallResponseHandler = async (request: Req
 
 export const subscriptionListSubmit: CallResponseHandler = async (request: Request, response: Response) => {
     let callResponse: AppCallResponse;
-		const call: AppCallRequest = request.body;
-		const i18nObj = configureI18n(call.context);
 
     try {
-        const integrations: WebhookSubscription[] = await subscriptionListCall(request.body);
-        const subscriptionsText: string = [
-            h6(i18nObj.__('api.subcription.list', { length: integrations.length.toString() })),
-            `${joinLines(
-                integrations.map((integration: WebhookSubscription) => 
-                    i18nObj.__('api.subcription.subcription_description', 
-												{ 
-														id: integration.id, 
-														service_name: integration.service!.name,
-														channel_name: integration.channel!.name
-												})
-                ).join('\n')
-            )}`
-        ].join('');
-
-        callResponse = newOKCallResponseWithMarkdown(subscriptionsText);
-        response.json(callResponse);
+        const message = await subscriptionListCall(request.body);
+        callResponse = newOKCallResponseWithMarkdown(message);
     } catch (error: any) {
         callResponse = showMessageToMattermost(error);
-        response.json(callResponse);
     }
+    response.json(callResponse);
 };
--- src/forms/subscription-list.ts
+++ src/forms/subscription-list.ts
@@ -12,12 +12,13 @@ import {configureI18n} from "../utils/translations";
 import {replace, tryPromiseForGenerateMessage} from '../utils/utils';
 import {MattermostClient, MattermostOptions} from "../clients/mattermost";
 import queryString, {ParsedUrl} from "query-string";
+import { h6, joinLines } from '../utils/markdown';
 
-export async function subscriptionListCall(call: AppCallRequest): Promise<WebhookSubscription[]> {
+export async function subscriptionListCall(call: AppCallRequest): Promise<string> {
     const mattermostUrl: string | undefined = call.context.mattermost_site_url;
     const userAccessToken: string | undefined = call.context.acting_user_access_token;
     const oauth2: Oauth2App | undefined = call.context.oauth2;
-		const i18nObj = configureI18n(call.context);
+    const i18nObj = configureI18n(call.context);
 
     const options: MattermostOptions = {
         mattermostUrl: <string>mattermostUrl,
@@ -34,24 +35,49 @@ export async function subscriptionListCall(call: AppCallRequest): Promise<Webhoo
     );
 
     const subscriptions: WebhookSubscription[] = response.data['webhook_subscriptions'];
-    const responseSubs: Promise<WebhookSubscription>[] = subscriptions.map(async (subs: WebhookSubscription) => {
+    
+    const responseSubs: Promise<WebhookSubscription | undefined>[] = subscriptions.map(async (subs: WebhookSubscription) => {
         const response: APIResponse = await tryPromiseForGenerateMessage(
             pdClient.get(replace(Routes.PagerDuty.ServicePathPrefix, Routes.PathsVariable.Identifier, subs.filter.id)),
             ExceptionType.MARKDOWN,
             i18nObj.__('forms.subcription.service-failed')
         );
+
         const service: Service = response.data['service'];
 
         const params: ParsedUrl = queryString.parseUrl(subs.delivery_method.url);
         const channelId: string = <string>params.query?.['channelId'];
-        const channel: Channel = await mattermostClient.getChannel(channelId);
-
-        return new Promise((resolve, reject) => resolve({
-            ...subs,
-            service,
-            channel
-        }));
+        try {
+            const channel: Channel = await mattermostClient.getChannel(channelId);
+            return new Promise((resolve, reject) => resolve({
+                ...subs,
+                service,
+                channel
+            }));
+        } catch (error) {
+            return Promise.resolve(undefined);
+        }
     });
 
-    return Promise.all(responseSubs);
+    const integrations: WebhookSubscription[] = webhookSubscriptionArray(await Promise.all(responseSubs).then());
+    
+    const subscriptionsText: string = [
+        h6(i18nObj.__('api.subcription.list', { length: integrations.length.toString() })),
+        `${joinLines(
+            integrations.map((integration: WebhookSubscription) =>
+                i18nObj.__('api.subcription.subcription_description',
+                    {
+                        id: integration.id,
+                        service_name: integration.service!.name,
+                        channel_name: integration.channel!.name
+                    })
+            ).join('\n')
+        )}`
+    ].join('');
+
+    return subscriptionsText;
+}
+
+function webhookSubscriptionArray(array: (WebhookSubscription | undefined)[]): WebhookSubscription[] {
+    return array.filter((el): el is WebhookSubscription => typeof (el) !== 'undefined');
 }
